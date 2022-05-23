0136e45e9d861447c363aadbffc7134a1463a5cb
i9bonsai <kupa.vx@gmail.com>
2022-05-24 09:46:36 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>i9bonsai</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial scale">
+        <link rel="icon" type="" href="">
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Hubballi&display=swap" rel="stylesheet">
+        <!-- font linking = font-family: 'Hubballi', cursive; -->
+    </head>
+    <body>
+        <div class="glass">
+            <img src="" alt="image">
+            <h1>i9bonsai</h1>
+        </div>
+        <div id="V1"></div>
+
+        
+
+        <script type="text/javascript" src="/js/particles.min.js"></script>
+        <script type="text/javascript" src="/js/custom.js"></script>
+        <?php
+        echo "testing";
+        ?>
+
+    </body>
+</html>
